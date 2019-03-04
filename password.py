bc22bd1b326a7b4f50859c56babf8b8c8418a8c9
April Tseng <aprilmow1018129@gmail.com>
2019-03-04 17:20:39 +0800

    change to while n>0

--- password.py
+++ password.py
@@ -1,7 +1,7 @@
 password = 'a123456'
 
 n = 3
-while True:
+while n > 0:
 	enter = input('enter your password:')
 	if enter == password:
 		print('login!')
@@ -9,5 +9,3 @@ while True:
 	else:
 		n = n - 1
 		print('wrong!', n, 'times left')
-		if n == 0:
-			break
