f7118bd1e052ab2410c596a7deb326efa7a51817
April Tseng <aprilmow1018129@gmail.com>
2019-03-04 17:27:03 +0800

    final version

--- password.py
+++ password.py
@@ -2,10 +2,14 @@ password = 'a123456'
 
 n = 3
 while n > 0:
+	n = n - 1
 	enter = input('enter your password:')
 	if enter == password:
 		print('login!')
 		break
 	else:
-		n = n - 1
-		print('wrong!', n, 'times left')
+		print('wrong!')
+		if n > 0:
+			print(n, 'times left')
+		else:
+			print('your account is blocked')
